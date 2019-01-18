6db6b715ae4451e44c1e8bdea691a62dcd1b09d7
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2019-01-18 17:38:46 -0500

    :hammer: Refactor of Map Visualizer

--- frontend/src/components/MapVisualizer/index.js
+++ frontend/src/components/MapVisualizer/index.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Stage, Layer, Circle, Line } from 'react-konva';
 
-export default class MapVisualizer extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  connectStops(leg) {
-    const startStop = this.props.store.stops.find( (stop) => stop.name === leg.startStop)
-    const endStop = this.props.store.stops.find( (stop) => stop.name === leg.endStop)
-    return <Line key={leg.legID} points={[startStop.x*7, startStop.y*7, endStop.x*7, endStop.y*7]} stroke="darkblue" strokeWidth="1"/>
-  }
-
-  render() {
-    const { legs, stops } = this.props.store
-    console.log(legs.length > 0 && stops.length > 0)
-    return (
-      <Stage width={200*2} height={150*5} className="ba">
-        <Layer>
-          {legs.map(leg => this.connectStops(leg))}
-          {stops.map(stop => <Circle key={stop.name} x={stop.x*7} y={stop.y*7} width={20} height={20} fill="darkblue" />)}
-        </Layer>
-      </Stage>
-    )
-  }
+const connectStops = (leg, stops) => {
+  const startStop = stops.find( (stop) => stop.name === leg.startStop)
+  const endStop = stops.find( (stop) => stop.name === leg.endStop)
+  return <Line key={leg.legID} points={[startStop.x*7, startStop.y*7, endStop.x*7, endStop.y*7]} stroke="darkblue" strokeWidth={1} />
 }
+
+const MapVisualizer = ({store}) => {
+  const { legs, stops } = store
+  return (
+    <Stage width={200*2} height={150*5} className="ba">
+      <Layer>
+        {legs.map(leg => connectStops(leg, stops))}
+        {stops.map(stop => <Circle key={stop.name} x={stop.x*7} y={stop.y*7} width={20} height={20} fill="darkblue" />)}
+      </Layer>
+    </Stage>
+  )
+}
+
+export default MapVisualizer
+
